fb770c0422aee07fb2cdc74f20ee890037e9d445
Glitch (hello-express) <none>
2019-01-06 11:20:19 +0000

    🛤📹 Checkpoint ./server.js:660488/1590 ./rules.js:660488/1694 ./grammar.js:660488/225

---
+++ grammar.js
@@ -0,0 +1,60 @@
+var dictionary = {
+  "origin":[
+    "[pad:#solidA#]#pad##pattern##pad#"
+  ],
+  "pattern":[
+    "[pAs:#solidA#][pa:#A#][pBs:#solidB#][pb:#B#]#pAs##pb##pa##pBs##center##pBs##pa##pb##pAs#",
+    "[pAs:#solidA#][pa:#A#][pBs:#solidB#][pb:#B#]#pAs##pb##pa##pb##pa##pBs##center##pBs##pa##pb##pa##pb##pAs#",
+    "[pa:#A#][pb:#B#]#pa##pa##pb##pa##pb##pb##center##pb##pb##pa##pb##pa##pa#",
+    "[pa:#A#][pb:#B#]#pa##pa##pa##pb##pa##pb##pa##pb##pb##pb##center##pb##pb##pb##pa##pb##pa##pb##pa##pa##pa#"
+  ],
+  "center":[
+    "#openchain#",
+    "#closedchain#",
+    "#blockchain#",
+    "#brokenchain#",
+    "#procchain#",
+    "#solidA#",
+    "[cAs:#solidA#][cBs:#solidB#]#cAs##cBs##cAs#"
+  ],
+  "openchain":[
+    "[oa:#A#]#oa##oa##B##oa##oa#"
+  ],
+  "closedchain":[
+    "[cca:#A#]#cca##cca##cca#"
+  ],
+  "blockchain":[
+    "[bca:#A#][bcb:#B#]#bca##bca##bca##bcb##bca##bcb##bca##bca##bca#"
+  ],
+  "brokenchain":[
+    "[bka:#A#][bkb:#B#]#bka##bkb##bkb##bkb##bka#"
+  ],
+  "procchain":[
+    "[pcAs:#solidA#][pcb:#B#]#pcAs##pcb##pcAs##pcb##pcAs#",
+    "[pca:#A#][pcBs:#solidB#]#pca##pcBs##pca#"
+  ],
+  "solidA":[
+    "[X:#A#]#solid#"
+  ],
+  "solidB":[
+    "[X:#B#]#solid#"
+  ],
+  "solid":[
+    "#X#",
+    "#X##X#",
+    "#X##X##X#",
+    "#X##X##X##X#",
+    "#X##X##X##X##X#"
+  ],
+  "A":["#color# "],
+  "B":["#color# "],
+  "color":[
+    "Red",
+    "Orange",
+    "Wheat",
+    "\\#202020",
+    "SteelBlue","DodgerBlue","CornflowerBlue","DarkOrchid","Violet","BlueViolet","Indigo","LightSalmon","FireBrick","Crimson","LightSkyBlue","LightYellow","White","Azure","Turquoise","PaleTurquoise","PeachPuff","RosyBrown","LightGreen","LightCyan","Honeydew","SteelBlue","DarkOrange","Olive"]};
+var tracery = require('tracery-grammar');
+var grammar = tracery.createGrammar(dictionary);
+grammar.addModifiers(tracery.baseEngModifiers);
+module.exports = grammar
--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const express = require('express');
 const app = express();
 
 const tracery = require('tracery-grammar');
-const rules = {"origin":["[pad:#solidA#]#pad##pattern##pad#"],"pattern":["[pAs:#solidA#][pa:#A#][pBs:#solidB#][pb:#B#]#pAs##pb##pa##pBs##center##pBs##pa##pb##pAs#","[pAs:#solidA#][pa:#A#][pBs:#solidB#][pb:#B#]#pAs##pb##pa##pb##pa##pBs##center##pBs##pa##pb##pa##pb##pAs#","[pa:#A#][pb:#B#]#pa##pa##pb##pa##pb##pb##center##pb##pb##pa##pb##pa##pa#","[pa:#A#][pb:#B#]#pa##pa##pa##pb##pa##pb##pa##pb##pb##pb##center##pb##pb##pb##pa##pb##pa##pb##pa##pa##pa#"],"center":["#openchain#","#closedchain#","#blockchain#","#brokenchain#","#procchain#","#solidA#","[cAs:#solidA#][cBs:#solidB#]#cAs##cBs##cAs#"],"openchain":["[oa:#A#]#oa##oa##B##oa##oa#"],"closedchain":["[cca:#A#]#cca##cca##cca#"],"blockchain":["[bca:#A#][bcb:#B#]#bca##bca##bca##bcb##bca##bcb##bca##bca##bca#"],"brokenchain":["[bka:#A#][bkb:#B#]#bka##bkb##bkb##bkb##bka#"],"procchain":["[pcAs:#solidA#][pcb:#B#]#pcAs##pcb##pcAs##pcb##pcAs#","[pca:#A#][pcBs:#solidB#]#pca##pcBs##pca#"],"solidA":["[X:#A#]#solid#"],"solidB":["[X:#B#]#solid#"],"solid":["#X#","#X##X#","#X##X##X#","#X##X##X##X#","#X##X##X##X##X#"],"A":["#color# "],"B":["#color# "],"color":["Red","Orange","Wheat","\\#202020","SteelBlue","DodgerBlue","CornflowerBlue","DarkOrchid","Violet","BlueViolet","Indigo","LightSalmon","FireBrick","Crimson","LightSkyBlue","LightYellow","White","Azure","Turquoise","PaleTurquoise","PeachPuff","RosyBrown","LightGreen","LightCyan","Honeydew","SteelBlue","DarkOrange","Olive"]};
-const grammar = tracery.createGrammar(rules);
-
+const grammar = require('./grammar.js');
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
